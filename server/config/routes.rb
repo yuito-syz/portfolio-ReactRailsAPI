c185035d6940b66c03c4b5a05d64764460f37eec
yuito-syz <yuito_sy1122@icloud.com>
2020-02-20 11:55:49 +0900

    Twitter認証の微調整

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations',
-    omniauth_callbacks: 'auth/omniauth_callbacks'
+    registrations: 'api/v1/auth/registrations',
+    omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
   }
   resources :todos
 end
